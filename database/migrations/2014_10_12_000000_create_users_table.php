e7a6ab14094507c77709b6cf59bf27659bfbc518
Phongphan Bunlom (WIN-PC) <phongphan.phb@mail.kmutt.ac.th>
2018-01-12 19:59:06 +0700

    [UPDATE] Edit `create_user_table` migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('validation_code');
+            $table->boolean('validation_status');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')
+            ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
