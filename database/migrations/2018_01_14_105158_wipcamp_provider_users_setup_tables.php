f0d1f550a87926089331a2cb177e482e399482dd
Phongphan Bunlom (WIN-PC) <phongphan.phb@mail.kmutt.ac.th>
2018-01-14 18:11:57 +0700

    [ADD] Create user authentication provider migration.

---
+++ database/migrations/2018_01_14_105158_wipcamp_provider_users_setup_tables.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class WipcampProviderUsersSetupTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::beginTransaction();
+
+        Schema::create('providers', function (Blueprint $table) {
+            $table->tinyIncrements('id')->unsigned();
+            $table->string('name', 32);
+            $table->string('display_name', 64)->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+        });
+
+        Schema::create('provider_users', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('user_id');
+            $table->unsignedTinyInteger('provider_id');
+            $table->string('provider_acc', 20);
+            $table->string('access_token');
+            $table->string('refresh_token')->nullable();
+            $table->dateTime('expired_at');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+
+            $table->unique(['user_id', 'provider_id']);
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('provider_id')->references('id')->on('providers')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+        DB::commit();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provider_users');
+        Schema::dropIfExists('providers');
+    }
+}
