612ad00ee64dc5b9ade5b8fead5087d1a2307cfc
ImagineRabbits <mixxdarkz2@gmail.com>
2018-01-18 17:37:35 +0700

    [UPDATE] fix string length error can't migrate database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->string('password');
             $table->string('validation_code');
             $table->boolean('validation_status');
