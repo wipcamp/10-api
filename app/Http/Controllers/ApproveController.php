341952b6fabc1bab5dc59ad3a7fd562ead377055
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-01-18 21:52:48 +0700

    [UPDATE] Add function with example return json

--- app/Http/Controllers/ApproveController.php
+++ app/Http/Controllers/ApproveController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class ApproveController extends Controller
 {
     //
+    function Index(){
+        return response()->json([
+            'status' => 200,
+            'message' => 'Hi! Approve',
+            'array' => [
+                '0' => ['id'=>0,'name'=>'farang','surname'=>'emmel'],
+                '1' => ['id'=>1,'name'=>'bas','surname'=>'tualek']
+            ]
+        ]); 
+    }
 }
