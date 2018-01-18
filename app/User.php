ec521acd7129f427ce38881d13f6dc4f6693b2cf
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2018-01-18 19:56:35 +0700

    [UPDATE] Edit User model.

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password',
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'validation_code', 'remember_token',
     ];
 }
