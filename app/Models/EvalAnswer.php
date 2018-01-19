3706a82e735538c26d20558c059d79a851e2e80d
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2018-01-19 16:16:04 +0700

    [ADD] Create EvalAnswer model.

---
+++ app/Models/EvalAnswer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EvalAnswer extends Model
+{
+    protected $guarded = [
+        'id', 'created_at', 'updated_at'
+    ];
+}
