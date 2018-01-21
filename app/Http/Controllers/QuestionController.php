3d74be717bbf6f3309ab87dc3bc2e55b32e370f3
ImagineRabbits <mixxdarkz2@gmail.com>
2018-01-21 20:15:36 +0700

    [ADD] Add QuestionController.php

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\QuestionRepository;
+
+class QuestionController extends Controller
+{
+    protected $questions;
+
+}
