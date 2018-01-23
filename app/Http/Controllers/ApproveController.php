83a81bb69583ad56018cb8fb3122c8756c53cb99
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-01-24 00:35:07 +0700

    [UPDATE] Add mock data

--- app/Http/Controllers/ApproveController.php
+++ app/Http/Controllers/ApproveController.php
@@ -28,7 +28,27 @@ class ApproveController extends Controller
         ]); 
     }
     function WithParams(Request $req){
-        return 'hello ' . $req->name . ' : ' . $req->surname;
+        if($req->name == 'ParentPermission'){
+            return response()->json([
+                status => 200,
+                data =>[
+                    '0' => ['user_id'=>10001,'camp_id'=>10,'type_id'=>1,'format_id'=>1,'path'=>'https://wip.camp/img/parent/12378912312.png','is_approve'=>0],
+                    '1' => ['user_id'=>10031,'camp_id'=>10,'type_id'=>1,'format_id'=>1,'path'=>'https://wip.camp/img/parent/87778732312.png','is_approve'=>1]
+                ]
+            ]);
+        }
+        else if($req->name == 'Transcript'){
+            return response()->json([
+                status => 200,
+                data =>[
+                    '0' => ['user_id'=>10001,'camp_id'=>10,'type_id'=>2,'format_id'=>1,'path'=>'https://wip.camp/img/transcript/12378912312.png','is_approve'=>0],
+                    '1' => ['user_id'=>10031,'camp_id'=>10,'type_id'=>2,'format_id'=>1,'path'=>'https://wip.camp/img/transcript/87778732312.png','is_approve'=>1]
+                ]
+            ]);
+        }
+        else{
+            return 'hello ' . $req->name . ' : ' . $req->surname;
+        }
     }
     function GetCheckTranscriptAmount(){
         return 31;
