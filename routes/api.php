4ee32e5a5e47970fb940e455c319bd16ff3dfc59
boat46450 <boat46450@hotmail.com>
2018-01-24 22:38:06 +0700

    [UPDATE] add problem type route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('/v1')->group(function () {
+    // API Report Problem
+    Route::prefix('/problemtype')->group(function () {
+        Route::get('/', 'ProblemTypeController@getAll');
+        Route::get('/{id}', 'ProblemTypeController@get');
+    });
+});
