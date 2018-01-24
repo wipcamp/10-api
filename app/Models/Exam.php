48302db93ddaa9b52846ab108a4e0f30c4092a23
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2018-01-24 19:58:06 +0700

    [UPDATE] Add `ProfileCamper` relationship to `Exam` model.

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -9,4 +9,9 @@ class Exam extends Model
     protected $guarded = [
         'created_at', 'updated_at'
     ];
+
+    public function profile_camper()
+    {
+        return $this->belongsTo('App\ProfileCamper', 'user_id', 'user_id');
+    }
 }
