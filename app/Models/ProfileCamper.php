fd3b4296a6ac639301d35ebd69c35009715522ed
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2018-01-24 20:03:05 +0700

    [UPDATE] Fix model namespace typo.

--- app/Models/ProfileCamper.php
+++ app/Models/ProfileCamper.php
@@ -11,11 +11,11 @@ class ProfileCamper extends Model
     ];
 
     public function profile() {
-        $this->belongsTo('App/Profile', 'user_id', 'user_id');
+        $this->belongsTo('App/Models/Profile', 'user_id', 'user_id');
     }
 
     public function camp_section() {
-        $this->belongsTo('App/CampSection', 'id', 'section_id');
+        $this->belongsTo('App/Models/CampSection', 'id', 'section_id');
     }
 
     public function exams() {
