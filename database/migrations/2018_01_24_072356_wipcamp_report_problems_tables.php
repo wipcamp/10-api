4caef1ea0e9103aee2a028da80f23f3ef64f602c
boat46450 <boat46450@hotmail.com>
2018-01-24 22:33:00 +0700

    [UPDATE] rename to wipcamp_report_problems

--- database/migrations/2018_01_24_072356_wipcamp_report_problems_tables.php
+++ database/migrations/2018_01_24_072356_wipcamp_report_problems_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateWipcampReportProblemsTable extends Migration
+class WipcampReportProblemsTables extends Migration
 {
     /**
      * Run the migrations.
@@ -46,6 +46,8 @@ class CreateWipcampReportProblemsTable extends Migration
             $table->foreign('report_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
         });
+
+        DB::commit();
     }
 
     /**
