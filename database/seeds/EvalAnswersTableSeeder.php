ef2fac9492f72b81471586d334e9443c3f74cead
ImagineRabbits <mixxdarkz2@gmail.com>
2018-01-26 00:53:30 +0700

    [UPDATE] Update user_id in EvalAnswer Seeder

--- database/seeds/EvalAnswersTableSeeder.php
+++ database/seeds/EvalAnswersTableSeeder.php
@@ -14,9 +14,9 @@ class EvalAnswersTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        for ($i=0; $i < 10; $i++) { 
+        for ($i=1; $i < 10; $i++) { 
             DB::table('eval_answers')->insert([
-                'user_id' => $faker->numberBetween($min = 1, $max = 10),
+                'user_id' => $i,
                 'question_id' => $faker->numberBetween($min = 1, $max = 6),
                 'data' => $faker->text
             ]);
