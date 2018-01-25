ddcca70ebf4193d86059e5120f6b59222d055062
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-01-25 21:45:09 +0700

    [UPDATE] finish return getProfileRegistrantAmountInToday

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\DashboardRepository;
 
 class DashboardController extends Controller
 {
     //
+    function Index ()
+    {
+        $dashboard = new DashboardRepository();
+        return $dashboard->getProfileRegistrantAmountInToday();
+    }
 }
