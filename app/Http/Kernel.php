71a98d9b4975406827b88d1c5c9dc5e9bfa0b1b0
ImagineRabbits <mixxdarkz2@gmail.com>
2018-02-02 14:36:18 +0700

    [ADD] Add JWT Config in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // jwt auth
+        'jwt-auth' => \App\Http\Middleware\JWTAuthentication::class,
     ];
 }
