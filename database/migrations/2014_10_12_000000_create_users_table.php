d48ee961cf1b010ff3791aac1b6e7e147ad2339f
ImagineRabbits <mixxdarkz2@gmail.com>
2018-02-02 14:43:04 +0700

    [REMOVE] Remove field name in migrate user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->string('name');
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')
