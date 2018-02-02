6224296147e4d8aaea9b694c39a125cbe4af8396
ImagineRabbits <mixxdarkz2@gmail.com>
2018-02-02 15:24:27 +0700

    [ADD] Add POST Users Route

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::prefix('/v1')->group(function () {
     // API User
     Route::prefix('/users')->group(function () {
         // API User with user_id
+        Route::post('/', 'UserController@create');
         Route::prefix('/{user_id}')->group(function () {
             Route::get('/answers/{question_id}', 'AnswerController@getById');
         });
