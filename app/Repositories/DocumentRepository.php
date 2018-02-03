574ec1966b73d833a46379d977fe0b99172a858a
Keerati <mixxdarkz2@gmail.com>
2018-02-04 01:09:53 +0700

    [ADD] Add Document Repository with create document

---
+++ app/Repositories/DocumentRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Document;
+use App\Repositories\DocumentTypeRepository;
+use App\Repositories\DocumentFormatRepository;
+
+class DocumentRepository implements ProviderUserRepositoryInterface {
+
+  public function create($userData) {
+    $document = new DocumentTypeRepository;
+    $documentFormat = new DocumentFormatRepository;
+    return Document::create([
+        'user_id' => $userData['userId'],
+        'type_id' => $document->getIdByName($userData['fileType']),
+        'format_id' => $documentFormat->getIdByName($userData['imageType']),
+        'path' => $userData['path']
+    ]);
+  }
+
+}
