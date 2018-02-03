0509e37e4c287cba45b3701910270376a56a445c
Keerati <mixxdarkz2@gmail.com>
2018-02-04 01:11:56 +0700

    [UPDATE] Update create in User Repository with Facebook Data

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,41 +1,46 @@
 <?php
 namespace App\Repositories;
+
 use App\User;
-use App\Models\Provider;
-use App\Models\ProviderUser;
-use App\Models\Document;
-use App\Models\DocumentFormat;
-use App\Models\DocumentType;
-use App\Models\Role;
-use App\Models\UserRole;
+
+use App\Repositories\UserRoleRepository;
+use App\Repositories\ProviderUserRepository;
+use App\Repositories\DocumentRepository;
 
 
 class UserRepository implements UserRepositoryInterface {
+
   public function create($data) {
     // constant
     $roleName = 'reg_registrants';
     $providerName = 'facebook';
+    $fileType = 'picture';
+    $imageUrl = $data['picture']['data']['url'];
+    $imageType = image_type_to_mime_type(exif_imagetype($imageUrl));
     // create
+    // user
     $user = User::create();
-    $userRole = UserRole::create([
-        'user_id' => $user->id,
-        'role_id' => Role::where('name',  $roleName)->first()->id
+    // user role
+    $userRole = new UserRoleRepository;
+    $userRole->create($user->id, $roleName);
+    // provider user
+    $providerUser = new ProviderUserRepository;
+    $providerUser = $providerUser->create([
+        'userId' => $user->id,
+        'accountName' => $data['name'],
+        'providerName' => $providerName,
+        'providerAcc' => $data['id'],
+        'accessToken' => $data['accessToken'],
+        'expiredIn' => $data['expiresIn']
     ]);
-    $providerUser = ProviderUser::create([
-        'user_id' => $user->id,
-        'account_name' => $data['name'],
-        'provider_id' => Provider::where('name', $providerName)->first()->id,
-        'provider_acc' => $data['id'],
-        'access_token' => $data['accessToken'],
-        'expired_in' => $data['expiresIn']
+    // document
+    $document = new DocumentRepository;
+    $document = $document->create([
+        'userId' => $user->id,
+        'fileType' => $fileType,
+        'imageType' => $imageType,
+        'path' => $imageUrl
     ]);
-    $document = Document::create([
-        'user_id' => $user->id,
-        'type_id' => DocumentType::where('name', 'image')->first()->id,
-        'format_id' => DocumentFormat::where('name', 'jpg')->first()->id,
-        'path' => $data['id']['data']['url']
-    ]);
-
 
     return json_encode(['result' =>
         [
