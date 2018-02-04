f0d6d3b6705cc0d4e45dc7be8f9ebf2b2020e8cf
Keerati <mixxdarkz2@gmail.com>
2018-02-04 19:47:16 +0700

    [ADD] Add User Controller with Powerful Create and JWT Login function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use JWTAuth;
+use JWTAuthException;
+
+use App\Repositories\UserRepository;
+
+class UserController extends Controller
+{
+
+    public function __construct() {
+        $this->user = new UserRepository;
+    }
+
+    public function create(Request $request) {
+        return $this->user->create($request->all());
+    }
+
+    public function get() {
+        // $this->user->create()
+        return 'Hi';
+    }
+    
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        $token = null;
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'response' => 'error',
+                    'message' => 'invalid_email_or_password',
+                ]);
+            }
+        } catch (JWTAuthException $e) {
+            return response()->json([
+                'response' => 'error',
+                'message' => 'failed_to_create_token',
+            ]);
+        }
+        return response()->json([
+            'response' => 'success',
+            'result' => [
+                'token' => $token,
+            ],
+        ]);
+    }
+
+    public function getAuthUser(Request $request) {
+        $user = JWTAuth::toUser($request->token);        
+        return response()->json(['result' => $user]);
+    }
+
+}
