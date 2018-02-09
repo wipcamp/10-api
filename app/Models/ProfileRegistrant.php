541366e9cd0c96dac20fd0ade7ced288e5f152f8
Keerati <mixxdarkz2@gmail.com>
2018-02-09 23:37:48 +0700

    [UPDATE] Update user_id in fillable

--- app/Models/ProfileRegistrant.php
+++ app/Models/ProfileRegistrant.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProfileRegistrant extends Model
 {
     protected $fillable = [
-        'addr_prov', 'addr_dist', 'telno_personal', 
+        'user_id', 'addr_prov', 'addr_dist', 'telno_personal', 
         'edu_name', 'edu_lv', 'edu_major', 'edu_gpax',
         'known_via', 'activities', 'skill_computer', 'past_camp',
         'parent_relation', 'telno_parent'
