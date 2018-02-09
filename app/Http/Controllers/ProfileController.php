ed39573f54c1e13f2d09c97feddcdf35e5b0f515
Keerati <mixxdarkz2@gmail.com>
2018-02-09 23:36:55 +0700

    [UPDATE] Add Validate and Edit Response Data

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,8 +14,44 @@ class ProfileController extends Controller
     }
 
     function create(Request $request) {
-        $data = $request->toArray();
-        return $this->profiles->create($data);
+        $schema = [
+            'user_id' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'first_name_en' => 'required',
+            'last_name_en' => 'required',
+            'nickname' => 'required',
+            'gender_id' => 'required',
+            'citizen_id' => 'required',
+            'religion_id' => 'required',
+            'birth_at' => 'required',
+            'blood_group' => 'required',
+            'congenital_diseases' => 'required',
+            'allergic_foods' => 'required',
+            'congenital_drugs' => 'required',
+            'addr_prov' => 'required',
+            'addr_dist' => 'required',
+            'telno_personal' => 'required',
+            'edu_name' => 'required',
+            'edu_lv' => 'required',
+            'edu_major' => 'required',
+            'edu_gpax' => 'required',
+            'parent_relation' => 'required',
+            'telno_parent' => 'required',
+        ];
+        // get data
+        $data = $request->all();
+        // validate
+        $validator = Validator::make($data, $schema);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Invalid Data.'
+            ]);
+        }
+        return response()->json([
+            'status' => 200,
+            'data' => $this->profiles->create($data)
+        ]);
     }
 
     function update(Request $request) {
