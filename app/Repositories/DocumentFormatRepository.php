637f8925a4998709a2cefc73dec3b50b8844c6ae
Keerati <mixxdarkz2@gmail.com>
2018-02-10 13:45:53 +0700

    [UPDATE] Update fine type

--- app/Repositories/DocumentFormatRepository.php
+++ app/Repositories/DocumentFormatRepository.php
@@ -6,7 +6,7 @@ use App\Models\DocumentFormat;
 class DocumentFormatRepository implements DocumentFormatRepositoryInterface {
 
   public function getIdByName($formatName) {
-    return DocumentFormat::where('name',  $formatName)->first()->id;
+    return DocumentFormat::where('name', $formatName)->orWhere('mime_type', $formatName)->first()->id;
   }
 
 }
