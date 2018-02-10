8c9764be47847fbe085bb0295856eba5a32cac38
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-02-11 03:01:42 +0700

    [UPDATE] add JWT Secert

--- writeENV.sh
+++ writeENV.sh
@@ -2,3 +2,4 @@ echo $GENERAL_ENV | tr " " "\n" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "APP_KEY=$APP_KEY" >> .env
 echo "SENTRY_DSN="$SENTRY_DSN >> .env
+echo "JWT_SECRET="$JWT_SECRET >> .env
