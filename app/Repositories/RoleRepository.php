bbc235d83b9a69753fa9592275fbbea5b611e240
Keerati <mixxdarkz2@gmail.com>
2018-02-18 01:16:32 +0700

    [ADD] Add Create Role in Role Repo

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -2,11 +2,19 @@
 namespace App\Repositories;
 
 use App\Models\Role;
+use App\Models\UserRole;
 
 class RoleRepository implements RoleRepositoryInterface {
 
   public function getIdByName($roleName) {
     return Role::where('name',  $roleName)->first()->id;
+  }
+
+  public function createStaff($id, $roleId) {
+    return UserRole::create([
+      'user_id' => $id,
+      'role_id' => $roleId
+    ]);
   }
 
 }
