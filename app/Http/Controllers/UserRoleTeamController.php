6ac02e865aada7679a013c510a5b8bc5d06c4449
boat46450 <boat46450@hotmail.com>
2018-02-23 00:13:31 +0700

    [UPDATE] get user role team by user_id

--- app/Http/Controllers/UserRoleTeamController.php
+++ app/Http/Controllers/UserRoleTeamController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\UserRoleTeamRepositoryInterface;
 
 class UserRoleTeamController extends Controller
 {
-    //
+    protected $userRoleTeamRepo;
+
+    public function __construct(UserRoleTeamRepositoryInterface $userRoleTeam) {
+        $this->userRoleTeamRepo = $userRoleTeam;
+    }
+
+    public function getByUserId($userId) {
+        $data = $this->userRoleTeamRepo->getByUserId($userId);
+        return json_encode($data);
+    }
 }
--- app/Repositories/UserRoleTeamRepository.php
+++ app/Repositories/UserRoleTeamRepository.php
@@ -1,6 +1,13 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\UserRoleTeam;
+
 class UserRoleTeamRepository implements UserRoleTeamRepositoryInterface {
+
+    public function getByUserId($userId) {
+        $data = UserRoleTeam::where('user_id', $userId)->get();
+        return $data;
+    }
 
 }
