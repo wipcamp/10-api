89fdf90dc49e9f12c0a8e2d5251165fe04d45f71
boat46450 <boat46450@hotmail.com>
2018-02-23 09:19:45 +0700

    [UPDATE] add timetable seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,7 +27,8 @@ class DatabaseSeeder extends Seeder
             DocumentTypesTableSeeder::class,
             DocumentFormatsTableSeeder::class,
             DocumentsTableSeeder::class,
-            RoleTeamsTableSeeder::class
+            RoleTeamsTableSeeder::class,
+            TimetablesTableSeeder::class
         ]);
     }
 }
