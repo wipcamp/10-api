f0cc89e1053e0b84124e4c9abf85f8459d9b88d1
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-03-21 02:36:27 +0700

    [UPDATE] Create Method getEvals and getEvalsByQuestionId

--- app/Repositories/EvalsRepository.php
+++ app/Repositories/EvalsRepository.php
@@ -1,7 +1,15 @@
 <?php
 namespace App\Repositories;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Evals;
 class EvalsRepository implements EvalsRepositoryInterface {
-    
+    protected $evals;
+    public function getEvals(){
+        $this->evals = new Evals();
+        return $this->evals->get();
+    }
+    public function getEvalsByQuestionId($questionId){
+        $this->evals = new Evals();
+        return $this->evals->where('question_id',$questionId)->get();
+    }
 }
