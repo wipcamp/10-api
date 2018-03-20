70a680771803424bb1bc3d01835fb46bcfef1750
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-20 23:20:02 +0700

    [ADD] question by team repository

--- app/Repositories/QuestionRepository.php
+++ app/Repositories/QuestionRepository.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Repositories;
 use App\Models\EvalQuestion;
+use Illuminate\Support\Facades\DB;
+
 
 class QuestionRepository implements QuestionRepositoryInterface {
     protected $questions;
@@ -18,5 +20,10 @@ class QuestionRepository implements QuestionRepositoryInterface {
             ->where('id', $question_id)
             ->get();
         return $result;
+    }
+
+    public function getByTeam($teamId)
+    {
+        return DB::select('select * from eval_questions where question_role_teams = '.$teamId);
     }
 }
