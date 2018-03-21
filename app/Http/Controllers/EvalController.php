089d74395f36bf8df58e978b75169c7715989091
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-21 22:54:41 +0700

    [ADD] EvalController

---
+++ app/Http/Controllers/EvalController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Repositories\EvalsRepository;
+class EvalController extends Controller
+{
+    //
+    function __construct() {
+        $this->evals = new EvalsRepository;
+    }
+    function Index ()
+    {
+        return response()->json([
+            'status'=>200,
+            'data'=>$this->evals->getEvals()
+        ]);
+    }
+    function getEvalsById ($questionId)
+    {
+        return response()->json($this->evals->getEvalsByQuestionId($questionId));
+    }
+}
