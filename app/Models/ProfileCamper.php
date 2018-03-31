c4a2598268197cdd3d011ac0b787dbc0bdd79c3b
Keerati <mixxdarkz2@gmail.com>
2018-03-31 17:50:33 +0700

    [UPDATE] Fix Path Model

--- app/Models/ProfileCamper.php
+++ app/Models/ProfileCamper.php
@@ -11,14 +11,14 @@ class ProfileCamper extends Model
     ];
 
     public function profile() {
-        $this->belongsTo('App/Models/Profile', 'user_id', 'user_id');
+        $this->belongsTo('App\Models\Profile', 'user_id', 'user_id');
     }
 
     public function camp_section() {
-        $this->belongsTo('App/Models/CampSection', 'id', 'section_id');
+        $this->belongsTo('App\Models\CampSection', 'id', 'section_id');
     }
 
     public function exams() {
-        $this->hasMany('App/Models/Exam', 'user_id', 'user_id');
+        $this->hasMany('App\Models\Exam', 'user_id', 'user_id');
     }
 }
