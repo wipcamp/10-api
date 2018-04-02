9a2f5e5baf7c27d52144776cdd7575e3bddcd011
ewqdsacxz <Patric_emmel@outlook.com>
2018-04-02 16:57:10 +0700

    [ADD] new Route

--- routes/api.php
+++ routes/api.php
@@ -250,5 +250,11 @@ Route::prefix('/v1')->group(function () {
             Route::get('/', 'NotificationController@getAll');
             Route::get('/user_id/{id}', 'NotificationController@getByUserId');
         });
+        //API Approve Slip
+        Route::prefix('slip')->group(function () {
+            Route::get('/all','SlipController@allCampers');//fetch camper and its slip
+            Route::get('/{docId}');//fetch document with camper detail
+            Route::put('/{docId}');//update document detail and comment
+        });
     });
 });
