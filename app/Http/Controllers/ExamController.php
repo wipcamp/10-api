0106753f1f8d613ba8c9ac4e87a985c12c41a953
Keerati <mixxdarkz2@gmail.com>
2018-05-20 17:32:18 +0700

    [UPDATE] Update Insert Answer for Exams with Validator

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
 use App\Repositories\ExamRepositoryInterface;
 
@@ -21,9 +22,28 @@ class ExamController extends Controller
     }
 
     function insertAnswer(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'data' => 'required|array'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Invalid Data.'
+            ]);
+        }
+
+        $answers = $request->get('data');
+        $result = null;
+
+        try {
+            $result = $this->exams->insertAnswer($answers);
+        } catch (Exception $err) {
+            $result = $err;
+        }
+        
         return response()->json([
             'status' => 200,
-            'data' => $this->exams->insertAnswer()
+            'data' => $result
         ]);
     }
 }
