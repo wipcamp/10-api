cfc9b29c3d104e59960e827fc3a595970a85c6fc
basinw <bas10600@gmail.com>
2018-05-26 03:12:10 +0700

    [UPDATE] Fix StaffRepositoryInterface

--- app/Repositories/StaffRepositoryInterface.php
+++ app/Repositories/StaffRepositoryInterface.php
@@ -4,6 +4,6 @@ namespace App\Repositories;
 interface StaffRepositoryInterface {
     public function getAll();
     public function getStaff($id);
-    public function create($id, $stdId);
+    public function create($id, $stdId, $flavorId);
     public function update($id, $section);
 }
