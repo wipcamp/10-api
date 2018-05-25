861469edd365963f6ba19b240a9711106adfffa8
Keerati <mixxdarkz2@gmail.com>
2018-05-26 01:13:41 +0700

    [UPDATE] Update api for create and update user for staff wipcamp

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::prefix('/v1')->group(function () {
     });
 
     Route::post('/profiles', 'ProfileController@create');
-    // ->middleware('checkCloseRegister')
+    // ->middleware('checkCloseRegister');
     // API Get and Create User
     Route::prefix('/users')->group(function () {
         Route::post('/', 'UserController@create');
@@ -71,10 +71,8 @@ Route::prefix('/v1')->group(function () {
         });
         // API Register
         Route::prefix('/profiles')->group(function () {
-            Route::post('/', 'ProfileController@create')
-            ->middleware('checkCloseRegister');
-            Route::put('/', 'ProfileController@update')
-            ->middleware('checkCloseRegister');
+            Route::put('/', 'ProfileController@update');
+            // ->middleware('checkCloseRegister');
             Route::get('/', 'ProfileController@get')
             ->middleware('checkWipperSpeacialByRole');
             Route::get('/{userId}', 'ProfileController@getProfile')
