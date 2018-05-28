57064731ff201f28e86160943ab6872ca89616d6
basinw <bas10600@gmail.com>
2018-05-29 06:26:02 +0700

    [UPDATE] Add route for create and update assign

--- routes/api.php
+++ routes/api.php
@@ -227,8 +227,10 @@ Route::prefix('/v1')->group(function () {
         });
 
         Route::prefix('/assigns')->group(function () {
+            Route::post('/', 'AssignController@createAssign');
             Route::get('/{id}', 'AssignController@getAssign');
             Route::get('/problem_id/{id}', 'AssignController@getByProblemId');
+            Route::put('/problem_id/{id} ', 'AssignController@updateAssign');
             Route::get('/role_team_id/{id}', 'AssignController@getByRoleTeamId');
             Route::get('/assigned_id/{id}', 'AssignController@getByAssignedId');            
         });
