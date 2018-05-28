f90d0a014d5f30dece28ff0efb7103719b4ed863
basinw <bas10600@gmail.com>
2018-05-29 06:27:02 +0700

    [UPDATE] Add function create and update in assign controller

--- app/Http/Controllers/AssignController.php
+++ app/Http/Controllers/AssignController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use App\Repositories\AssignRepositoryInterface;
 
 class AssignController extends Controller
@@ -16,6 +18,51 @@ class AssignController extends Controller
     public function getAssign($assignId) {
         $data = $this->assignRepo->getAssign($assignId);
         return json_encode($data);
+    }
+
+    public function createAssign(Request $request) {
+        $data = $request->all();
+        $result = array_get($data, 'data');
+        
+        $temp = true;
+        foreach($result as $obj) {
+            $schema = [
+                'problem_id' => 'required',
+                'role_team_id', // role_team
+                'assigned_id' // wipid
+            ];
+            $validator = Validator::make($obj, $schema);
+            if ($validator->fails()) {
+                $temp = false;
+                return response()->json(false);
+            }
+            $this->assignRepo->create($obj);
+        }
+        
+        return response()->json($temp);
+    }
+
+    public function updateAssign(Request $request, $id) {
+        $data = $request->all();
+        $result = array_get($data, 'data');
+
+        $this->assignRepo->deleteByProblemId($id);
+        $temp = true;
+        foreach($result as $obj) {
+            $schema = [
+                'problem_id' => 'required',
+                'role_team_id', // role_team
+                'assigned_id' // wipid
+            ];
+            $validator = Validator::make($obj, $schema);
+            if ($validator->fails()) {
+                $temp = false;
+                return response()->json(false);
+            }
+            $this->assignRepo->create($obj);
+        }
+        
+        return response()->json($temp);
     }
 
     public function getByProblemId($problemId) {
